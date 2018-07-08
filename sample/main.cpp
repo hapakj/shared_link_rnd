5b2fbb216f283904a931e957ee10738e292e8a7b
jozsef.hapak <jozsef.hapak@kishonti.net>
2018-07-08 15:39:01 +0200

    create sample and static_lib a

---
+++ sample/main.cpp
@@ -0,0 +1,8 @@
+#include "static_a.h"
+
+int main(int argc, char ** argv)
+{
+	static_a();
+	return 0;
+}
+
---
+++ static_lib_a/static_a.cpp
@@ -0,0 +1,9 @@
+#include "static_a.h"
+
+#include <iostream>
+
+void static_a()
+{
+	std::cout << "static_lib_a" << std::endl;
+}
+
